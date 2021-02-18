4e8ea1e2d8e206d8b1288ccadf81364f94224403
Brend-Support <79204947+Brend-Support@users.noreply.github.com>
2021-02-19 00:15:23 +0300

    Update liste.py

--- userbot/modules/liste.py
+++ userbot/modules/liste.py
@@ -1,11 +1,3 @@
-# Copyright (C) 2020 Yusuf Usta.
-#
-# Licensed under the GPL-3.0 License;
-# you may not use this file except in compliance with the License.
-#
-
-# Asena UserBot - Yusuf Usta
-
 from userbot import CMD_HELP
 from userbot.events import register
 from PIL import Image
