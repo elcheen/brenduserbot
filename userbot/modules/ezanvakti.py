513985f5616516869328eaeab26798c8d6b14824
Brend-Support <79204947+Brend-Support@users.noreply.github.com>
2021-02-19 00:06:51 +0300

    Update ezanvakti.py

--- userbot/modules/ezanvakti.py
+++ userbot/modules/ezanvakti.py
@@ -1,14 +1,3 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-
-# Asena UserBot - Yusuf Usta
-
-# @NaytSeyd tarafından portlanmıştır.
-# @frknkrc44 tarafından düzenlenmiştir.
-
 import requests
 from userbot import CMD_HELP
 from userbot.events import register
