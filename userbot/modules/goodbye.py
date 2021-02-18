10f01ac03302e41002174821d9905051010391de
Brend-Support <79204947+Brend-Support@users.noreply.github.com>
2021-02-19 00:12:30 +0300

    Update goodbye.py

--- userbot/modules/goodbye.py
+++ userbot/modules/goodbye.py
@@ -1,12 +1,3 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-
-# Asena UserBot - Yusuf Usta
-
-
 from userbot.events import register
 from userbot import CMD_HELP, bot, LOGS, CLEAN_WELCOME, BOTLOG_CHATID
 from telethon.events import ChatAction
