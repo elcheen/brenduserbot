24e488b720ac9ee4ea71e48fda2efe0a1a7d561d
Brend-Support <79204947+Brend-Support@users.noreply.github.com>
2021-02-19 00:11:06 +0300

    Update gdrive.py

--- userbot/modules/gdrive.py
+++ userbot/modules/gdrive.py
@@ -1,12 +1,3 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-
-# Asena UserBot - ᴇʟçɪɴ ¦ 🇯🇵 ⁪⁬⁮⁮⁮⁮ 
-
-
 import asyncio
 import math
 import os
