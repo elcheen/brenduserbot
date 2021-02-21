3dacb0f9e7e6b88b9e9a6018c7e2cbc936372021
brendsupport <79204947+brendsupport@users.noreply.github.com>
2021-02-21 18:25:25 +0400

    Update __asena.py

--- userbot/modules/__asena.py
+++ userbot/modules/__asena.py
@@ -1,14 +1,3 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-
-# Asena UserBot - Yusuf Usta
-#
-
-""" UserBot yardım komutu """
-
 from userbot.cmdhelp import CmdHelp
 from userbot import cmdhelp
 from userbot import CMD_HELP
@@ -17,13 +6,13 @@ from userbot.events import register
 # ██████ LANGUAGE CONSTANTS ██████ #
 
 from userbot.language import get_value
-LANG = get_value("__asena")
+LANG = get_value("__brend")
 
 # ████████████████████████████████ #
 
-@register(outgoing=True, pattern="^.asena(?: |$)(.*)")
+@register(outgoing=True, pattern="^.brend(?: |$)(.*)")
 async def asena(event):
-    """ .asena komutu için """
+    
     args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
