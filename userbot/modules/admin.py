be136bd0e380d5019bdf33d81d3c783c94727892
brendsupport <79204947+brendsupport@users.noreply.github.com>
2021-02-21 18:28:26 +0400

    Update admin.py

--- userbot/modules/admin.py
+++ userbot/modules/admin.py
@@ -1,16 +1,3 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-
-# Asena UserBot - Yusuf Usta
-#
-
-"""
-Grup yönetmenize yardımcı olacak UserBot modülü
-"""
-
 from asyncio import sleep
 from os import remove
 
