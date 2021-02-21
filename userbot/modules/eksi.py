f0698dda160bbdcb7f0489d17f8ed5faaaa40854
brendsupport <79204947+brendsupport@users.noreply.github.com>
2021-02-21 17:26:55 +0400

    Update eksi.py

--- userbot/modules/eksi.py
+++ userbot/modules/eksi.py
@@ -1,40 +1,32 @@
-# Copyright (C) 2021 ᴇʟçɪɴ ¦ 🇯🇵
-#
-# Licensed under the GPL-3.0 License;
-# you may not use this file except in compliance with the License.
-#
-
-# Brend UserBot - ᴇʟçɪɴ ¦ 🇯🇵
-
 from userbot.events import register
 from eksipy import Baslik, Giri, Eksi
 from datetime import datetime
 import urllib.parse
 from userbot.cmdhelp import CmdHelp
 
-@register(outgoing=True, pattern="^.başlıq(\d*) ?(.*)")
+@register(outgoing=True, pattern="^.başlık(\d*) ?(.*)")
 async def baslik(event):
-    səhifə = event.pattern_match.group(1)
-    if səhifə == '':
-        səhifə = 1
+    sayfa = event.pattern_match.group(1)
+    if sayfa == '':
+        sayfa = 1
     else:
-        səhifə = int(sayfa)
+        sayfa = int(sayfa)
 
-    başlıq = event.pattern_match.group(2)
+    baslik = event.pattern_match.group(2)
     try:
-        başlıq = Başlıq(başlıq, səhifə)
+        baslik = Baslik(baslik, sayfa)
     except:
-        return await event.edit('`Belə bir başlıq tapılmadı.`')
+        return await event.edit('`Böyle bir başlık bulunamadı.`')
     
-    topic = başlıq.get_topic()
-    entrys = başlıq.get_entrys()
-    Result = f'**Başlıq: **`{topic.title}`\n`{topic.current_page}/{topic.max_page}`\n\n'
+    topic = baslik.get_topic()
+    entrys = baslik.get_entrys()
+    Result = f'**Başlık: **`{topic.title}`\n`{topic.current_page}/{topic.max_page}`\n\n'
     
     for entry in entrys:
         if len(entry.text().strip()) < 450:
             Result += f'`{entry.text().strip()}`\n__[{datetime.utcfromtimestamp(entry.date).strftime("%d/%m/%Y")}](https://eksisozluk.com/entry/{entry.id}) [{entry.author}](https://eksisozluk.com/biri/{urllib.parse.quote(entry.author)})__\n\n'
         else:
-            Result += f'**Bu entry uzun görünür.** `.entry {entry.id}` ile ala bilərsiniz.\n\n'
+            Result += f'**Bu entry uzun gözüküyor.** `.entry {entry.id}` ile alabilirsiniz.\n\n'
     return await event.edit(Result)
 
 @register(outgoing=True, pattern="^.entry ?(\d*)")
@@ -43,34 +35,34 @@ async def entry(event):
     try:
         Entry = Giri(Entry).get_entry()
     except:
-        return await event.edit('`Belə bir entry tapılmadı.`')
+        return await event.edit('`Böyle bir entry bulunamadı.`')
     
-    Result = f'**Başlıq: **`{Entry.topic.title}`\n\n'
+    Result = f'**Başlık: **`{Entry.topic.title}`\n\n'
     Result += f'`{Entry.text().strip()}`\n __[{datetime.utcfromtimestamp(Entry.date).strftime("%d/%m/%Y")}](https://eksisozluk.com/entry/{Entry.id}) [{Entry.author}](https://eksisozluk.com/biri/{urllib.parse.quote(Entry.author)})__\n\n'
     return await event.edit(Result)
 
-@register(outgoing=True, pattern="^.g[üu]ndəm ?(\d*)$")
-async def gundəm(event):
+@register(outgoing=True, pattern="^.g[üu]ndem ?(\d*)$")
+async def gundem(event):
     if event.pattern_match.group(1) == '':
-        Səhifə = 1
+        Sayfa = 1
     else:
-        Səhifə = int(event.pattern_match.group(1))
+        Sayfa = int(event.pattern_match.group(1))
 
     try:
-        Gündəm = Eksi().gundem(Səhifə)
+        Gundem = Eksi().gundem(Sayfa)
     except:
-        return await event.edit('`Bir xəta baş verdi.`')
+        return await event.edit('`Bir hata oluştu.`')
     
     Result = ""
     i = 1
     for Baslik in Gundem:
-        Result += f'`{i}-)` [{Başlıq.title}]({Başlıq.url()}) __{Başlıq.giri}__\n'
+        Result += f'`{i}-)` [{Baslik.title}]({Baslik.url()}) __{Baslik.giri}__\n'
     return await event.edit(Result)
 
-CmdHelp('köhnə').add_command(
-    'başlıq', '<səhifə> <başlıq', 'Ekşi Sözlükte başlıq gətirər.', 'başlıq2 php'
+CmdHelp('eksi').add_command(
+    'baslik', '<sayfa> <başlık>', 'Ekşi Sözlükte başlık getirir.', 'baslik2 php'
 ).add_command(
-    'entry', '<id>', 'Entry gətirər.', 'entry 1'
+    'entry', '<id>', 'Entry getirir.', 'entry 1'
 ).add_command(
-    'gundem', '<səhifə'>, 'Gündəm gətirər.', 'gündəm 1'
+    'gundem', '<sayfa>', 'Gündem getirir.', 'gündem 1'
 ).add()
